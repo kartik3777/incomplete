e565f9157ed09fd172e1c474fd03b5b8363e8fe3
Prabhat220774 <121498880+Prabhat220774@users.noreply.github.com>
2024-04-12 04:24:41 +0530

    Update NewProject.jsx

--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import { Link,useNavigate } from "react-router-dom";
 import './about.css';
+import Loader from '../components/Faculty/Loader';
 
 function NewProject(props) {
   const navigate = useNavigate();
 
+  const [facultyData, setFacultyData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: '',
     description: '',
@@ -16,9 +19,50 @@ function NewProject(props) {
     resumerequired: 'yes', 
     maxstudents: ''
   });
+  const [saving, setSaving] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://cs253backederror404teamnotfoundmohammaadnasarsiddiqui.vercel.app/api/user/faculty/projects/`
+        );
+        // console.log(response.data);
+        setFacultyData(response.data); 
+        setLoading(false); 
+        // Assuming the response contains an array of faculty data
+      } catch (error) {
+        console.error("Error fetching faculty data:", error);
+        setLoading(false); 
+      }
+    };
+
+    fetchData();
+  }, []);
+
+       // Create a Set to store unique projectCategory values
+  const uniqueCategoriesSet = new Set();
+
+// Iterate through the facultyData array
+ facultyData.forEach((item) => {
+   if(item.projectCategory){
+    uniqueCategoriesSet.add(item.projectCategory);
+   }
+ 
+ });
+
+// Convert the Set back to an array
+const uniqueCategoriesArray = Array.from(uniqueCategoriesSet);
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    if(name==="maxstudents"){
+      e.target.value = `${value}`;
+    }
+    if(name==="cpirequired"){
+      e.target.value = `${value}`;
+    }
     setFormData({
       ...formData,
       [name]: value
@@ -28,27 +72,59 @@ function NewProject(props) {
 
 
   const handleSubmit = async (e) => {
-
     e.preventDefault();
+    if(
+      !formData.name ||
+      !formData.description ||
+      !formData.cpirequired ||
+      !formData.prereg ||
+      !formData.openfor ||
+      !formData.projectCategory ||
+      !formData.resumerequired ||
+      !formData.maxstudents 
+    ){
+      alert("Please fill all details."); 
+      return;
+    }
+    
+    const floatValue = parseFloat(formData.cpirequired);
+    
+    if (isNaN(floatValue) || floatValue < 0 || floatValue > 10) {
+        alert('Please enter a valid CPI between 0 and 10.');
+        return; 
+    }
+    const num = parseInt(formData.maxstudents);
+    if(!(Number.isInteger(num) && num > 0)){
+      alert('Total students must be a valid positive integer.');
+      return;
+    }
+
+   // e.preventDefault();
     try {
+      setSaving(true);
       const uniqueID = props.profId;
-      console.log("uniqueId reached to newPorject is: "+ uniqueID);
+      //console.log("uniqueId reached to newPorject is: "+ uniqueID);
       const url = `https://cs253backederror404teamnotfoundmohammaadnasarsiddiqui.vercel.app/api/professor/${uniqueID}/createproject`;
 
      
       const response = await axios.post(url, formData);
   
       if (response.status === 201) {
-        console.log('Project created successfully');
+       // console.log('Project created successfully');
         navigate('/Project_list_prof');
       } else {
-        console.error('Failed to create project');
+       // console.error('Failed to create project');
         alert("error occured");
       }
     } catch (error) {
       console.error('Error creating project:', error.message);
+    }finally {
+      setSaving(false); 
     }
   };
+  if (loading) {
+    return <Loader  />;
+  }
 
 
   return (
@@ -75,7 +151,7 @@ function NewProject(props) {
           />
           <label id='kartik' className='project-form-label' htmlFor="minimum-cpi">Minimum Cpi:</label>
           <input
-            type="text"
+            type="number"
             id="minimum-cpi"
             name="cpirequired"
             onChange={handleChange}
@@ -96,13 +172,26 @@ function NewProject(props) {
             onChange={handleChange}
           />
           <label id='kartik' className='project-form-label' htmlFor="category">Category: </label>
-          <input
+          <select id="category" name="projectCategory" onChange={handleChange}>
+              <option  value="">Select Category</option>
+               {uniqueCategoriesArray.map((category, index) => {
+                  if (category.length <70) {
+                   return <option key={index} value={category}>{category}</option>;
+                  }
+                  return null;
+               })}
+               <option value="other">Other(Custom)</option>
+          </select>
+
+          {formData.projectCategory === "other" && (
+            <input
             type="text"
-            id="category"
-            name="projectCategory"
-            placeholder="e.g. Machine Learning"
+            id="other-category"
+            name="otherCategory"
+            placeholder="Enter custom category"
             onChange={handleChange}
-          />
+            />
+          )}
           <label id='kartik' className='project-form-label' htmlFor="resume">Resume:</label>
           <select
             id="resume"
@@ -115,7 +204,7 @@ function NewProject(props) {
 
           <label id='kartik' className='project-form-label' htmlFor="max-students">Total Students</label>
           <input
-            type="text"
+            type="number"
             id="max-students"
             name="maxstudents"
             onChange={handleChange}
@@ -128,7 +217,15 @@ function NewProject(props) {
           </button>
           </Link>
           
-           <input type="submit" className="submit-button" value="Save" />
+          <input
+              type="submit"
+              className="submit-button"
+              placeholder='bhai'
+              value={saving ? (
+                "Saving..."
+              ) : "Save"}
+              disabled={saving}
+            />
            </div>
           
         </form>
