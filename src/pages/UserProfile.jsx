ba45608c96c5fb31b08e34388b81f2fe4d093086
Prabhat220774 <121498880+Prabhat220774@users.noreply.github.com>
2024-04-12 03:58:58 +0530

    Update UserProfile.jsx

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -2,34 +2,69 @@ import React, { useState, useEffect } from 'react';
 // import img from '../components/Faculty/users.png';
 import { useNavigate } from 'react-router-dom';
 import './UserProfileCss.css';
+import axios from 'axios';
+import Loader from '../components/Faculty/Loader';
 import UserDetails from './userdetails'
 
-function ProfileCard(props) {
-    const navigate = useNavigate();
+const UserProfile=(props)=> {
 
-    const handleDetailsClick =() => {
-        navigate('/ProjectDesc');
-    };
-//     return (
-//     <div className="profile-container100">
-//       <div className="profile-info100">
-//         <h2>My Profile</h2>
-//         <p><strong>Name:</strong> {props.name}</p>
-//         <p><strong>Roll No:</strong> {props.rollno}</p>
-//         <p><strong>Email:</strong> {props.email}</p>
-//         <p><strong>LinkedIn Profile:</strong> <a href={props.resume} className="profile-linkedin" target="_blank" rel="noopener noreferrer">Visit Profile</a></p>
-//       </div>
-//     </div>
-//   ); 
+    const [user, setUser] = useState({
+        name: '',
+        rollno: '',
+        email: '',
+        resume: '',
+        arrRequested:[],
+        arrAccepted:[],
+        arrRejected:[]
+    }) 
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const uniqueId = props.logedInStudentData._id;
+            const url = `https://cs253backederror404teamnotfoundmohammaadnasarsiddiqui.vercel.app/api/user/getuserinfo/${uniqueId}`;
+            const response = await axios.get(url);
+      
+            // Handle the fetched data (e.g., update state or perform other actions)
+            //console.log(response);
+      
+            setUser({
+              name: response.data.user.name,
+              rollno: response.data.user.rollno,
+              email: response.data.user.email,
+              resume:response.data.user.resumeLink,
+              arrRequested: response.data.user.projectsRequested,
+              arrAccepted: response.data.user.projectsEnrolled,
+              arrRejected:response.data.user.projectsRejected
+            });
+           // console.log(user);
+            setLoading(false);
+           
+          } catch (error) {
+            console.error('Error fetching student profile:', error);
+            setLoading(false);
+           
+          }
+        };
+      
+        fetchData(); // Call the fetchData function when the component mounts
+      }, [props.logedInStudentData._id]);
+
+      if (loading) {
+        return <Loader/>; 
+      }
+    
    return (
+    <div className='flexcard8'>
         <div className="profile-container100">
             <div className="profile-section100">
                <h2>Personal Information</h2>
                 <div className="profile-info100">
-                   <p><strong>Name:</strong> {props.name}</p>
-                   <p><strong>Roll No:</strong> {props.rollno}</p>
-                   <p><strong>Email:</strong> {props.email}</p>
-                   <p><strong>Resume Link:</strong> <a href={props.resume} className="profile-linkedin" target="_blank" rel="noopener noreferrer">See Resume</a></p>
+                   <p><strong>Name:</strong> {user.name}</p>
+                   <p><strong>Roll No:</strong> {user.rollno}</p>
+                   <p><strong>Email:</strong> {user.email}</p>
+                   <p><strong>Resume Link:</strong> <a href={user.resume} className="profile-linkedin" target="_blank" rel="noopener noreferrer">See Resume</a></p>
                 </div>
             </div>
             <div className="profile-section100">
@@ -44,7 +79,7 @@ function ProfileCard(props) {
                         </tr>
                     </thead>
                     <tbody>
-                        {props.arrAccepted.map((project,index) => (
+                        {user.arrAccepted && user.arrAccepted.map((project,index) => (
                             <tr key={index+1}>
                                 <td>{index+1}</td>
                                 <td>{project.name}</td>
@@ -52,17 +87,17 @@ function ProfileCard(props) {
                                 <td>{project.offeredByProf.name} {/*props.arrAccepted.offeredByProf.name*/ }</td>
                             </tr>
                         ))}
-                        {props.arrRequested.map((project,index) => (
-                            <tr key={index+1+props.arrAccepted.length}>
-                                <td>{index+1+props.arrAccepted.length}</td>
+                        {user.arrRequested && user.arrRequested.map((project,index) => (
+                            <tr key={index+1+user.arrAccepted.length}>
+                                <td>{index+1+user.arrAccepted.length}</td>
                                 <td>{project.name}</td>
                                 <td>Pending</td>
                                 <td>{project.offeredByProf.name}</td>
                             </tr>
                         ))}
-                        {props.arrRejected.map((project,index) => (
-                            <tr key={index+1+props.arrAccepted.length+props.arrRequested.length}>
-                                <td>{index+1+props.arrAccepted.length+props.arrRequested.length}</td>
+                        {user.arrRejected && user.arrRejected.map((project,index) => (
+                            <tr key={index+1+user.arrAccepted.length+user.arrRequested.length}>
+                                <td>{index+1+user.arrAccepted.length+user.arrRequested.length}</td>
                                 <td>{project.name}</td>
                                 <td>Rejected</td>
                                 <td>{project.offeredByProf.name} {/*props.arrRejected.offeredByProf.name*/ }</td>
@@ -72,30 +107,9 @@ function ProfileCard(props) {
                 </table>
             </div>
         </div>
+        </div>
     );
 }
 
-
-function UserProfile(props){
-  const  myData = props.logedInStudentData
-    //   console.log("data in userProfile: "+myData);
-      console.log(myData);
-
-    return (
-        <div className='flexcard8'>
-            
-                   <ProfileCard  
-                    name  ={myData.name}
-                    email = {myData.email}
-                    //image = {item.image}
-                   resume ={myData.resumeLink}
-                    rollno={myData.rollno}
-                    arrRequested={myData.projectsRequested}
-                    arrAccepted={myData.projectsEnrolled}
-                    arrRejected={myData.projectsRejected}
-                   />
-        </div>
-      )
-}
 
 export default UserProfile;
